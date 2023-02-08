2beba716e81c4e86ed0b7b05c0597e1a7c6e78f1
ganeugen <ganeugen@gmail.com>
2023-02-08 22:21:22 +0100

    send notification funciton

--- telegramBot/main.py
+++ telegramBot/main.py
@@ -1,9 +1,11 @@
 import asyncio
 import os
+import time
 from datetime import datetime
 
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
+from aiogram.types import ParseMode
 from telegramBot.config import API_TOKEN
 from aiogram.utils import executor
 from telegramBot.config import URL_APP
@@ -19,6 +21,16 @@ dp = Dispatcher(bot)
 async def on_startup(dp):
     print('Bot is online')
     await bot.set_webhook(URL_APP)
+    while True:
+        now = datetime.utcnow()
+        subscribers = db.get_subscriptions()
+        for subscriber in subscribers:
+            await bot.send_message(
+                chat_id=subscriber,
+                text=f'Here is your periodic message.{now}',
+                parse_mode=ParseMode.HTML,
+            )
+        time.sleep(10)
 
 
 # don't forget to close the db (cursor and database)
@@ -26,24 +38,10 @@ async def on_shutdown(dp):
     await bot.delete_webhook()
     await db.close()
 
-
-async def sendNotification():
-    now = datetime.utcnow()
-    await bot.send_message(db.get_subscriptions(), f"{now}")
-
-
 client.register_handlers_client(dp)
 other.register_handlers_other(dp)
 
-scheduler = AsyncIOScheduler()
-scheduler.add_job(sendNotification, 'interval', seconds=10)
-scheduler.start()
-
-
-
-
 executor.start_webhook(
-    loop=scheduler,
     dispatcher=dp,
     webhook_path='',
     on_startup=on_startup,
