cf214a0716049b4c79136370e037431b0feebea0
ganeugen <ganeugen@gmail.com>
2023-02-16 04:59:10 +0100

    added context menager for "createTable" function

--- telegramBot/dataBase/dataBase.py
+++ telegramBot/dataBase/dataBase.py
@@ -38,7 +38,7 @@ def create_table(tableName, colString):
     logging.debug(f'Table name: {tableName}; \n Column string: {colString}')
 
     with connection:
-        cursor.execute("drop table if exists %s", (tableName,))
+        cursor.execute("drop table if exists %s;", (tableName,))
         cursor.execute("create table %s (%s);", (tableName, colString))
 
 
